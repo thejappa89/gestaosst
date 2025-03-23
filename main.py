d0bfb7602737bacee364b781bbd1592772640e5a
Henrique Lima <jappadev@gmail.com>
2025-03-22 23:35:36 -0300

    Refatorando codigo

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# Author: Henrique Lima
-# Repository: githubr.com/thejappa89/gestaosst
+from jappalibs.interface import *
 
-print("""
-   _____           _                 _____ _____ _______ 
-  / ____|         | |               / ____/ ____|__   __|
- | |  __  ___  ___| |_ __ _  ___   | (___| (___    | |   
- | | |_ |/ _ \/ __| __/ _` |/ _ \   \___ \\___ \    | |   
- | |__| |  __/\__ \ || (_| | (_) |  ____) |___) |  | |   
-  \_____|\___||___/\__\__,_|\___/  |_____/_____/   |_|   
-""")
-print('Sistem de Gestão em Saúde e Segurança do Trabalho'.center(56))
+cabecario()
