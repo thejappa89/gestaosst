a008a726c95ddb7fe79874fdac60568ba325f63e
Henrique Lima <jappadev@gmail.com>
2025-03-23 22:27:00 -0300

    [STYLE] Adicionado novas funções

--- jappalibs/interface.py
+++ jappalibs/interface.py
@@ -1,9 +1,9 @@
-def linha(caracter='-', tamanho=60):
+def linha(caracter='-', tamanho=42):
     print(caracter * tamanho)
 
 
 def cabecario():
-    linha()
+    linha(tamanho=68)
     print("""
    _____           _                 _____ _____ _______ 
   / ____|         | |               / ____/ ____|__   __|
@@ -13,6 +13,34 @@ def cabecario():
   \_____|\___||___/\__\__,_|\___/  |_____/_____/   |_|   
 """)
     print('Sistem de Gestão em Saúde e Segurança do Trabalho'.center(56))
-    linha()
+    linha(tamanho=68)
     print()
-cabecario()
+
+
+def leiaint(msg):
+    while True:
+        try:
+            numero = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: Por favor, digite um número inteiro válido!\33[m]]')
+        except KeyboardInterrupt:
+            print('\033[31m\nEntrada de lib interrompida pelo usuário\033[m')
+            return 0
+        else:
+            return numero
+
+def titulo(txt):
+    linha()
+    print(txt.center(42))
+    linha()
+
+
+def menu(lista, msg='MENU PRINCIPAL'):
+    titulo(msg)
+
+    for e, item in enumerate(lista):
+        print(f'[{e + 1}] {item}')
+    linha()
+
+    opc = leiaint('Sua opção: ')
+    return opc
